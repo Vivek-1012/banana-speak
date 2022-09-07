cd8fb620a267c8f268bfec03c4d7831e6462d99f
Vivek-1012 <110529479+Vivek-1012@users.noreply.github.com>
2022-09-07 19:24:28 +0530

    doc

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 var btnTranslate = document.querySelector("#btn-Translate");
 var txtInput = document.querySelector("#txt-input"); 
 var outputDiv = document.querySelector("#output");
+ 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
+function getTranslationURL(input) {
+     return serverURL + "?" + "text=" + input
+}
 
 function clickHandler() {
-    outputDiv.innerText = "ajsjsjsjsjsjsj " + txtInput.value;
+    // outputDiv.innerText = "ajsjsjsjsjsjsj " + txtInput.value;
+   var inputText = txtInput.value;
+    
+   //calling server for processing
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json => {
+    var translatedText = json.contents.translated;
+    outputDiv.innerText = translatedText;
+   })
+    
    
 };
 
